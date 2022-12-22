ed1976f636d4b1655673077544b94b2e84f35131
Pallavic5 <pchedge235@gmail.com>
2022-12-22 18:27:28 +0530

    Added: Logic of Ability to user need to enter a valid last name.

--- src/com/bridgelabz/regex/UserDetailsValidation.java
+++ src/com/bridgelabz/regex/UserDetailsValidation.java
@@ -1,14 +1,15 @@
 package com.bridgelabz.regex;
 /*
- * Problem Statement :UC1 As a User need to enter a valid First Name
- * - First name starts with Cap and has minimum 3 characters
+ * Problem Statement :UC2 As a User need to enter a valid Last Name 
+ * - Last name starts with Cap and has minimum 3 characters
+ * 
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserDetailsValidation {
 	/*
-	 * regex pattern for firstname
+	 * regex pattern for lastname
 	 * Pattern class defines a pattern to be used in search
 	 * Matcher class used to search for the pattern
 	 * Boolean matches test whether the regular expression matches the pattern.
@@ -24,5 +25,16 @@ public class UserDetailsValidation {
 			System.out.println("invalid");
 		}
 	}
+	
+	public static void isValidLastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
 
+		if (matcher.matches()) {
+			System.out.println("valid");
+		} else {
+			System.out.println("invalid");
+		}
+	}
 }
--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -1,24 +1,32 @@
 package com.bridgelabz.regex;
 /*
- * Problem Statement : As a User need to enter a valid First Name
- * - First name starts with Cap and has minimum 3 characters
+ * Problem Statement :UC2 As a User need to enter a valid Last Name 
+ * - Last name starts with Cap and has minimum 3 characters
+ * - 
  */
 import java.util.Scanner;
 
 public class UserRegistration {
 	static Scanner scanner = new Scanner(System.in);
+	//method for check first name
 	public static void checkUserFirstName() {
-		System.out.println("Enter a First Name:");
+		System.out.print("Enter a First Name:");
 		String firstName = scanner.next();
 		UserDetailsValidation.isValidFirstName(firstName);
+	}
+	//method for check last name
+	public static void checkUserlastName() {
+		System.out.print("Enter a Last Name: ");
+	    String lastName = scanner.next();
+	    UserDetailsValidation.isValidLastName(lastName);
 	}
 		/*
 		 * main method
 		 */
 	public static void main(String[] args) {
 		System.out.println("Welcome To UserRegistration Problem.");
+		//methods calling
 		checkUserFirstName();
-		scanner.close();
+		checkUserlastName();
 	}
-
 }
